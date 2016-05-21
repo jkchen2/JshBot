8f4f247050787e0632718a9ec23e2f29e8cad814
jkchen2 <jkchen2@illinois.edu>
2016-05-20 22:53:59 -0500

    start.py change
    The file is now read from the start file, and not internally from the module. Also, added the config file update for an upcoming core.py change.

--- start.py
+++ start.py
@@ -4,4 +4,4 @@ if len(sys.argv) == 2 and sys.argv[1] == 'debug':
     use_debug = True
 else:
     use_debug = False
-jshbot.core.initialize(debug=use_debug)
+jshbot.core.initialize(__file__, debug=use_debug)
