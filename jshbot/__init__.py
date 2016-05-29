634bb778e17e0275ff3afefc684d0bc63da07981
jkchen2 <jkchen2@illinois.edu>
2016-05-28 21:31:57 -0500

    Removed servers.py
    Bot should be fully functional without servers.py, now replaced with data.py.

--- jshbot/__init__.py
+++ jshbot/__init__.py
@@ -5,7 +5,6 @@ import jshbot.core as core
 import jshbot.exceptions as exceptions
 import jshbot.parser as parser
 import jshbot.plugins as plugins
-import jshbot.servers as servers
 import jshbot.commands as commands
 import jshbot.data as data
 
