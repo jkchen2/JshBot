d50e9f544bcb897981ca3862c31e09a2aa689bf3
jkchen2 <jkchen2@illinois.edu>
2016-06-09 17:39:56 -0500

    Configurations changes
    Data for plugins that does not need to be changed directly through the bot is now put int he same folder as regular configurations.

--- jshbot/configurations.py
+++ jshbot/configurations.py
@@ -1,11 +1,9 @@
-import discord
-import logging
 import json
-import os.path
 
 from jshbot.exceptions import BotException, ErrorTypes
 
 EXCEPTION = 'Configurations'
+
 
 def get_configurations(bot):
     configurations_list = {}
@@ -14,9 +12,10 @@ def get_configurations(bot):
         with open(directory + '/config.json', 'r') as config_file:
             configurations_list['core'] = json.load(config_file)
     except Exception as e:
-        raise BotException(EXCEPTION,
-                "Could not open the core configuration file", e=e,
-                error_type=ErrorTypes.STARTUP)
+        raise BotException(
+            EXCEPTION,
+            "Could not open the core configuration file", e=e,
+            error_type=ErrorTypes.STARTUP)
 
     directory += '/'
     for plugin in bot.plugins:
@@ -24,14 +23,41 @@ def get_configurations(bot):
             with open(directory + plugin + '.json', 'r') as config_file:
                 configurations_list[plugin] = json.load(config_file)
         except FileNotFoundError:
-            if (getattr(bot.plugins[plugin][0] ,'uses_configuration', False) and 
-                    bot.plugins[plugin][0].uses_configuration):
-                raise BotException(EXCEPTION,
-                        "Plugin " + plugin + " requires a configuration file, "
-                        "but it was not found.", error_type=ErrorTypes.STARTUP)
+            module = bot.plugins[plugin][0]
+            if (getattr(module, 'uses_configuration', False) and
+                    module.uses_configuration):
+                raise BotException(
+                    EXCEPTION, "Plugin {} requires a configuration file, "
+                    "but it was not found.".format(plugin),
+                    error_type=ErrorTypes.STARTUP)
         except Exception as e:
-            raise BotException(EXCEPTION,
-                    "Could not open the " + plugin + " configuration file.",
-                    e=e, error_type=ErrorTypes.STARTUP)
+            raise BotException(
+                EXCEPTION, "Could not open the {} configuration file.".format(
+                    plugin), e=e, error_type=ErrorTypes.STARTUP)
 
     return configurations_list
+
+
+def get(bot, plugin_name, extra=None, extension='json'):
+    """Gets the configuration file for the given plugin.
+
+    Keyword arguments:
+    extra -- Looks for <plugin_name>-<extra>.<extension>
+    extension -- If 'json', reads the file as json, otherwise reads it as text.
+    """
+    if extra:
+        filename = '{0}/config/{1}-{2}.{3}'.format(
+            bot.path, plugin_name, extra, extension)
+    else:
+        return bot.configurations[plugin_name]
+    try:
+        with open(filename, 'r') as config_file:
+            if extension.lower() != 'json':
+                return config_file.read()
+            else:
+                return json.load(config_file)
+    except FileNotFoundError:
+        raise BotException(EXCEPTION, "File {} not found.".format(filename))
+    except Exception as e:
+        raise BotException(
+            EXCEPTION, "Failed to read {} properly.".format(filename), e=e)
