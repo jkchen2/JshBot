5c36971b62ad96f702de490a7cd7737932ae0057
jkchen2 <jkchen2@illinois.edu>
2016-06-10 22:46:32 -0500

    Configurations improvement
    The get command now can accept a key and has exception handling.

--- jshbot/configurations.py
+++ jshbot/configurations.py
@@ -5,7 +5,7 @@ from jshbot.exceptions import BotException, ErrorTypes
 EXCEPTION = 'Configurations'
 
 
-def get_configurations(bot):
+def add_configurations(bot):
     configurations_list = {}
     directory = bot.path + '/config'
     try:
@@ -35,13 +35,15 @@ def get_configurations(bot):
                 EXCEPTION, "Could not open the {} configuration file.".format(
                     plugin), e=e, error_type=ErrorTypes.STARTUP)
 
+    bot.configurations = configurations_list
     return configurations_list
 
 
-def get(bot, plugin_name, extra=None, extension='json'):
+def get(bot, plugin_name, key=None, extra=None, extension='json'):
     """Gets the configuration file for the given plugin.
 
     Keyword arguments:
+    key -- Gets the specified key from the config file.
     extra -- Looks for <plugin_name>-<extra>.<extension>
     extension -- If 'json', reads the file as json, otherwise reads it as text.
     """
@@ -49,13 +51,28 @@ def get(bot, plugin_name, extra=None, extension='json'):
         filename = '{0}/config/{1}-{2}.{3}'.format(
             bot.path, plugin_name, extra, extension)
     else:
-        return bot.configurations[plugin_name]
+        try:
+            config = bot.configurations[plugin_name]
+        except KeyError:
+            raise BotException(
+                EXCEPTION,
+                "Plugin {} not found in the configurations dictionary.".format(
+                    plugin_name))
+        try:
+            if key:
+                return config[key]
+            else:
+                return config
+        except KeyError:
+            raise BotException(
+                EXCEPTION,
+                "Key {} not found in the configuration file.".format(key))
     try:
         with open(filename, 'r') as config_file:
-            if extension.lower() != 'json':
-                return config_file.read()
-            else:
+            if extension.lower() == 'json':
                 return json.load(config_file)
+            else:
+                return config_file.read()
     except FileNotFoundError:
         raise BotException(EXCEPTION, "File {} not found.".format(filename))
     except Exception as e:
