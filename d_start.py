34cb7e2b301f2e6617e685b1f3c8ee9fe3dfa0a7
jkchen2 <jkchen2@illinois.edu>
2017-06-23 05:24:30 -0500

    Start file updates
    Now that sharding is automatic, there is no more need for a sharding argument.
    
    Also, added a new special start file for use when JshBot is deployed using Docker.

---
+++ d_start.py
@@ -0,0 +1,3 @@
+# Startup file for use when deployed from Docker
+import jshbot
+jshbot.core.start()
--- start.py
+++ start.py
@@ -1,21 +1,8 @@
 import jshbot
 import sys
 
-if 'debug' in sys.argv:
+use_debug = 'debug' in sys.argv
+if use_debug:
     print("Saving debug prints to logs.txt")
-    use_debug = True
-else:
-    use_debug = False
 
-if 'shards' in sys.argv:
-    try:
-        total_shards = int(sys.argv[sys.argv.index('shards') + 1])
-        assert total_shards > 1
-        print("Using {} shards".format(total_shards))
-    except:
-        print("Invalid shard number - must be greater than 1.")
-        sys.exit(1)
-else:
-    total_shards = 1
-
-jshbot.core.initialize(__file__, debug=use_debug, shards=total_shards)
+jshbot.core.start(__file__, debug=use_debug)
