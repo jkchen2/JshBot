c7993c873d2213d82b10bbb9cd90c6089eee6308
jkchen2 <jkchen2@illinois.edu>
2021-04-06 02:06:56 -0500

    Clean up discord.py dependency

--- setup.py
+++ setup.py
@@ -1,10 +1,5 @@
 from setuptools import setup, find_packages
 
-# discord.py dependency is kind of hack-y due to limitations in setup
-# If you want to be pure about it, change the version in
-#   the dependency_links entry from 10 to 1.0.0a0 or whatever version
-#   and the same in install_requires from >0.9 to ==version
-
 setup(
     name='jshbot',
     author='Jsh',
@@ -12,11 +7,8 @@ setup(
     version='0.4.0a1',
     packages=find_packages(),
     license='MIT',
-    dependency_links=[
-        'https://github.com/Rapptz/discord.py/archive/master.zip#egg=discord.py-10'
-    ],
     install_requires=[
-        'discord.py[voice]>0.9',
+        'discord.py[voice]',
         'pyyaml',
         'psycopg2-binary'
     ]
